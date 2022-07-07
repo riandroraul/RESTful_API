65835b5786202c53ea8df6c7f050923416e6944d
riandroraul <riandroraul@gmail.com>
2022-07-07 15:45:09 +0700

    menggunakan express-rate-limit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { getAllBooks, addBooks, getBookById, updateBook, deleteBook } = require('
 const rateLimit = require('express-rate-limit')
 const limiter = rateLimit({
     windowMs: 5 * 60000, // 5 minutes, 60000 = 1 minutes
-    max: 5 * 2,
+    max: 5 * 2, // 5x hit
     message: 'too many request, wait a second!!!'
 });
 
@@ -11,62 +11,10 @@ const router = express.Router();
 router.use(limiter)
 
 router.get('/books', limiter, getAllBooks)
-    /**
-     * @swagger
-     * /books
-     * get: 
-     *  ringkasan: mengembalikan semua daftar buku 
-     *  response: 
-     *  200: 
-     *   content: 
-     *     aplication/json:
-     *       schema:
-     *         type: array
-     *           items: 
-     *             $ref: '#components/schemas/Book'
-     */
+router.get('books/id/:id', limiter, getBookById)
+router.post('books/tambah', limiter, addBooks)
+router.put('books/ubah/:id', limiter, updateBook)
+router.delete('books/hapus/:id', limiter, deleteBook)
 
-router.get('/book/id/:id', limiter, getBookById)
-
-/**
- * @swagger
- * /books/id/{id}:
- * get: 
- *  ringkasan: mengembalikan data buku berdasarkan  id
- *  tags: [books]
- *  parameters: 
- *     in: path
- *     name: id
- *     schema: 
- *         type: string
- *         required: true
- *   description: id buku
- *     response: 
- *       200: 
- *         description: data buku berdasarkan id
- *         content: 
- *           aplication/json:
- *             schema:
- *                $ref: '#components/schemas/Book'
- *       404:
- *         description: data buku berdasarkan id
- */
-router.post('/tambah', limiter, addBooks)
-router.put('/ubah/:id', limiter, updateBook)
-router.delete('/hapus/:id', limiter, deleteBook)
-
-// router.put('/books/ubah', (req, res) => {
-//   res.json({
-//     status: 'ok',
-//     message: 'berhasil',
-//   })
-// });
-
-// router.delete('/books/hapus', (req, res) => {
-//   res.json({
-//     status: 'ok',
-//     message: 'berhasil',
-//   })
-// })
 
 module.exports = router
